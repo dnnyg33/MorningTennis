a75d04adcd3da29c041950509a1dd7cc4df28d32
Daniel George <>
2021-11-19 14:29:19 -0700

    rename sort functions. add messaging through fcm add sunpro field

--- functions/src/index.js
+++ functions/src/index.js
@@ -4,7 +4,7 @@ admin.initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.sortWeek2 = functions.database.ref("/incoming/{day}").onWrite((snapshot, context) =>
+exports.sortWeek = functions.database.ref("/incoming/{day}").onWrite((snapshot, context) =>
 {
     const original = snapshot.after.val()
     var key = context.params.day
@@ -12,6 +12,50 @@ exports.sortWeek2 = functions.database.ref("/incoming/{day}").onWrite((snapshot,
     var groups = tennisSort(original)
     return admin.database().ref("/sorted").child(key).set(groups)
 });
+
+exports.sortWeekv2 = functions.database.ref("/incoming-v2/{day}").onWrite((snapshot, context) =>
+{
+    const original = snapshot.after.val()
+    var key = context.params.day
+        
+    var groups = tennisSort(original)
+    return admin.database().ref("/sorted-v2").child(key).set(groups)
+});
+
+exports.scheduleOpenNotification = functions.pubsub.schedule('00 8 * * FRI')
+.timeZone('America/Denver')
+.onRun((context) => {
+    //todo read db
+    const registrationTokens = ["e_hniT70Ql-rzt7iUN73kE:APA91bHlfC-s3_RiXrE8iOVOgYSBXMFlT5_5qQ3422_iusXWZUgfnM-1gWrNdOxd9PUXJuyAnncbPJL6yUa6Vwaf8WsiINBNIeTtD8f7pnnOZHHcxnZx9qT-Tf0H79qpYi5HrwCJLC0C"]
+    const message = {
+        "notification": {
+            "title": "Schedule now open",
+            "body": "You can now sign up for next week's schedule in the app."
+        },
+        "tokens": registrationTokens,
+      };
+    sendNotificationsToGroup(message, registrationTokens)
+});
+
+function sendNotificationsToGroup(message, registrationTokens, res) {
+    admin.messaging().sendMulticast(message)
+        .then((response) => {
+          if (response.failureCount > 0) {
+            const failedTokens = [];
+            response.responses.forEach((resp, idx) => {
+                console.log(resp)
+              if (!resp.success) {
+                failedTokens.push(registrationTokens[idx]);
+              }
+            });
+            console.log('List of tokens that caused failures: ' + failedTokens);
+            res.end('List of tokens that caused failures: ' + failedTokens)
+          } else {
+              console.log("No errors sending messages")
+              res.end("No errors sending messages")
+          }
+        })
+}
 
 function tennisSort(data) {
     let uniqueData = removeDuplicates(data)
@@ -110,8 +154,17 @@ function buildSortedObjectFull(day, item, choice) {
     if (item.phoneNumber != undefined) {
         phoneNumber = item.phoneNumber 
     } 
-    return {"day": day, "name": item.name + " ("+ choice+")", "phoneNumber": phoneNumber}
+    var hasSunpro = false
+    console.log("Item.hasSunpro " + item.sunPro)
+    if (item.sunPro === "Yes") {
+        hasSunpro = true
+    }
+    return {"day": day, "name": item.name + " ("+ choice+")", "phoneNumber": phoneNumber, "hasSunpro": hasSunpro}
 }
 function buildSortedObject(pair) {
-    return {"name": pair.name, "phoneNumber": pair.phoneNumber}
+    var name = pair.name
+    if (pair.hasSunpro) {
+        name = "*" + pair.name
+    }
+    return {"name": name, "phoneNumber": pair.phoneNumber}
 }
