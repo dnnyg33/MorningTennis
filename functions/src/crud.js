4b68b5e3503641b736b5bd91fada3d566df7755c
Daniel George <danielsgeorge@gmail.com>
2025-09-12 13:28:20 -0600

    add createGroup endpoint

--- functions/src/crud.js
+++ functions/src/crud.js
@@ -2,7 +2,8 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const index = require('./index.js')
 const notifications = require('./notifications.js')
-const utr = require('./utr_updates.js')
+const utr = require('./utr_updates.js');
+const { schedule } = require("firebase-functions/v1/pubsub");
 module.exports.processLateSubmission = processLateSubmission;
 module.exports.createUser = createUser;
 module.exports.joinGroupRequest = joinGroupRequest;
@@ -13,6 +14,7 @@ module.exports.modifyGroupMember = modifyGroupMember;
 module.exports.inviteUserToGroup = inviteUserToGroup;
 module.exports.deleteAccount = deleteAccount;
 module.exports.deleteGroup = deleteGroup;
+module.exports.createGroup = createGroup;
 
 
 /**
@@ -535,6 +537,56 @@ function deleteAccount(req, res) {
         res.send({ "data": { "result": "success", "log": removedLog } })
     });
 };
+
+async function createGroup(req, res) {
+    const body = req.body;
+    console.log("body: " + JSON.stringify(body))
+    if (body.group == null || body.firebaseId == null || body.publicUserId == null) {
+        res.status(400).send({ "data": { "result": "failure", "reason": "group, firebaseId and publicUserId are required" } })
+        return;
+    }
+    //validate properties on group
+    if (body.group.name == null || body.group.id == null || body.group.scheduleIsOpen == null
+        || body.group.visibility == null || body.group.meetups2 == null || body.group.sortingAlgorithm == null
+    ) {
+        res.status(400).send({ "data": { "result": "failure", "reason": "group.name, group.id, group.scheduleIsOpen, group.visibility, group.meetups2 and group.sortingAlgorithm are required" } })
+        return;
+    }
+    //inspect that all meetups2 are valid
+    const validDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+    for (const meetup of body.group.meetups2) {
+        if (meetup.dayOfWeek == null || meetup.dayOfWeek < 0 || meetup.dayOfWeek > 6 || !validDays.includes(meetup.dayOfWeek)) {
+            res.status(400).send({ "data": { "result": "failure", "reason": "all meetups2 must valid dayOfWeek" } })
+            return;
+        }
+    }
+
+    const newGroup = {
+        name: body.group.name,
+        id: body.group.id,
+        description: body.group.description,
+        location: body.group.location,
+        visibility: body.group.visibility,
+        dateCreated: new Date().getTime(),
+        admins: { 'creator': body.firebaseId },
+        scheduleIsOpen: body.group.scheduleIsOpen,
+        code: body.group.code,
+        sortingAlgorithm: body.group.sortingAlgorithm,
+        memberCount: 1,
+        meetups2: body.group.meetups2,
+    }
+    const newGroupRef = admin.database().ref("groups-v2").child(body.group.id).set(newGroup);
+    //add group to user's list of groups
+    await admin.database().ref("approvedNumbers").child(body.firebaseId).child('groups').once('value').then((snapshot) => {
+        const groups = snapshot.val() || [];
+        groups.push(body.group.id);
+        console.log("groups: " + JSON.stringify(groups))
+        return admin.database().ref("approvedNumbers").child(body.firebaseId).child('groups').set(groups);
+    });
+    //create member_ranking for this user
+    await admin.database().ref("member_rankings").child(body.group.id).child(body.publicUserId).set({ "utr": 4, "goodwill": 1, 'suspended': false });
+    res.status(200).send({ "data": { "result": "success", "groupId": body.group.id, "group": newGroup } })
+}
 
 async function deleteGroup(req, res) {
     const db = admin.database();
